7e2db7a578e57e175ea180f63d23d919360e4f47
Salvador <salvaadr0@gmail.com>
2026-01-20 14:38:40 +0100

    Hasta el apartado 3 todo hecho

--- src/main/java/org/iesalixar/daw2/sdr/dwese2526_ticket_logger_webapp_sdr/config/SecurityConfig.java
+++ src/main/java/org/iesalixar/daw2/sdr/dwese2526_ticket_logger_webapp_sdr/config/SecurityConfig.java
@@ -55,7 +55,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> {
                     logger.debug("Configurando autorización de solicitudes HTTP");
                     auth
-                            .requestMatchers("/", "/js/**", "/css/**", "/images/**", "/login", "/register").permitAll()        // Acceso anónimo
+                            .requestMatchers("/", "/js/**", "/css/**", "/images/**", "/login", "/register", "/auth/**", "/error", "/error/**").permitAll()        // Acceso anónimo
                             .requestMatchers("/users**").hasRole("ADMIN")         // Solo ADMIN
                             // REGIONS: ADMIN o MANAGER (para algunas pruebas de permisos)
                             .requestMatchers("/regions**").hasAnyRole("ADMIN", "MANAGER")
